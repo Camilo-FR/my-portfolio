c42d9fab4c7734985b7132b39ff04a03c7b102be
Camille VACHER <camille.vacher@live.fr>
2023-06-01 16:25:56 +0200

    finalizing contact page before responsive

--- js/contact.js
+++ js/contact.js
@@ -19,3 +19,19 @@ function textFunc() {
 }
 
 timer1 = setInterval("textFunc()", 150); // Every 150 milliseconds
+
+// modal contact icones
+
+const imgPhone = document.querySelector(".img-phone");
+const imgEmail = document.querySelector(".img-email");
+const modalPhone = document.querySelector(".phone-modal");
+const modalEmail = document.querySelector(".email-modal");
+
+imgPhone.addEventListener("click", () => {
+  console.log("click on phone");
+  modalPhone.classList.toggle("show");
+});
+
+imgEmail.addEventListener("click", () => {
+  modalEmail.classList.toggle("show");
+});
